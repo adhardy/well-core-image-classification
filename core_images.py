71eed205eeb7b3f5320421571406549c512e1529
Adam Hardy <adh167@gmail.com>
2021-06-07 10:20:09 +0100

    implemented slice image labelling

--- core_images.py
+++ core_images.py
@@ -1,16 +1,18 @@
 import os
 from PIL import Image 
 import numpy as np
+import pandas as pd
 
 class CoreImages():
 
-    def __init__(self, core_photos, core_x, core_y, core_x_mm, slice_window, slice_step=None):
+    def __init__(self, core_photos:list, core_x:list, core_y:list, core_x_mm:int, slice_window:int, slice_step:int=None) -> None:
         """
         core_photos: a list of core image paths
         core_x: Tuple (x1,x2) of the x coordinates of the core trays
         core_x_cm: length of the core in cm
         core_y: Tuple of tuples ((y1,y2),(y1,y2),(y1,y2),...) with y coordinates for each of the cores within the image
-        slice: width to slice each core image into. If not integer divisible by the length of the core, the end of the core will be discarded
+        slice_window: width (px) to slice each core image into. If not integer divisible by the length of the core, the end of the core will be discarded
+        slice_step: the interval (px) to move when taking a new slice. Minimum of 1px, defaults to value of slice_window. 
         """
 
         self.slice_window = slice_window
@@ -36,36 +38,45 @@ class CoreImages():
         self.paths = [] #stores paths of all photos, cores, slices
         self.px_cm_conversion = core_x_mm / self.core_length #get conversion factor between pixels and cm
 
-    def px_to_mm(self, px):
+    def px_to_mm(self, px:int)->int:
         """ Converts a number of pixels to a measurement in cm"""
         return px * self.px_cm_conversion
     
-    def mm_to_px(self, cm):
+    def mm_to_px(self, cm:int)->int:
         """ Converts a measurment of cm to a number in pixels"""
         return cm / self.px_cm_conversion
 
-    def __getitem__(self, idx):
-        #still a bit broken, needs work to retrieve these problem
+    def __getitem__(self, idx:int)-> list:
+        """return the relative paths of the core images"""
+        #still a bit broken, needs work to retrieve these properly
         return self.paths[idx]
 
-    def slice_cores(self, core_dir = os.getcwd(), slice_dir = os.getcwd(), verbose = 0):
+    def slice_cores(self, core_dir:str=os.getcwd(), slice_dir:str=os.getcwd(), labels:str=None, verbose:int=0) -> None:
         """extract the cores from each image, and slice the cores into slices of <slice> width"""
         self.core_slices = []
         core_left, core_right = self.core_x
 
-        for n_photo, core_photo in enumerate(self.core_photos):
+        if labels:
+            df_labels = pd.read_csv(labels)
+            if verbose:
+                print("Core labels:")
+                print(df_labels)
+
+        for core_photo in self.core_photos:
+
+            photo_ID = os.path.splitext(os.path.basename(core_photo))[0]
+
             if verbose > 0:
                 print(f"Processing core photo: {core_photo}")
+
             core_paths = []
             for n_core in range(self.cores_per_image):
                 #extract the core from the image
                 core_photo_img = Image.open(core_photo)
                 core_img = core_photo_img.crop((core_left,self.core_y[n_core][0],core_right,self.core_y[n_core][1]))
-                core_path = f"{core_dir}/{os.path.splitext(os.path.basename(core_photo))[0]}_{n_core}.jpg"
-                
+                core_path = f"{core_dir}/{photo_ID}_{n_core}.jpg"
                 core_img.save(core_path)
                 
-
                 #slice the core up
                 core_width, core_height = core_img.size
                 slice_left = 0
@@ -73,8 +84,14 @@ class CoreImages():
                 n_slice = 0
                 slice_paths = []
                 while slice_right < core_width:
+
+                    # find the labels for this slice
+                    if labels:
+                        length_cm = max(self.px_to_mm(slice_left+self.slice_window/2),0) #add half the window size to find the label at the midpoint of the window
+                        label = (df_labels[(df_labels["photo_ID"] == photo_ID) & (df_labels["n_core"] == n_core+1) & (df_labels["length"] <= length_cm)].tail(1)["type"].values)[0]
+
                     slice_img = core_img.crop((slice_left,0,slice_right,core_height))
-                    slice_path = f"{slice_dir}/{os.path.splitext(os.path.basename(core_photo))[0]}_{n_core}_{n_slice}_{{0:05.0f}}_{{1:05.0f}}.jpg".format(self.px_to_mm(slice_left)*10,self.px_to_mm(slice_right)*10)
+                    slice_path = f"{slice_dir}/{photo_ID}_{n_core}_{n_slice}_{{0:05.0f}}_{{1:05.0f}}_{label}.jpg".format(self.px_to_mm(slice_left)*10,self.px_to_mm(slice_right)*10)
                     slice_paths.append(slice_path)
                     slice_img.save(slice_path)
                     slice_img.close()
